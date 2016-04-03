02d6658aabb4d3b849fe2805eafd66c2e5846283
jianhao1203 <MOIMAC@Mewaholeos-MacBook-Pro.local>
2016-04-03 14:00:16 +0800

    asdas
    as dad

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,15 +1,10 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
-        "id": "cordova-plugin-whitelist.whitelist",
-        "runs": true
-    },
-    {
-        "file": "plugins/phonegap-plugin-push/www/push.js",
-        "id": "phonegap-plugin-push.PushNotification",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
         "clobbers": [
-            "PushNotification"
+            "device"
         ]
     },
     {
@@ -24,13 +19,6 @@ module.exports = [
         "id": "cordova-plugin-dialogs.notification_android",
         "merges": [
             "navigator.notification"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-device/www/device.js",
-        "id": "cordova-plugin-device.device",
-        "clobbers": [
-            "device"
         ]
     },
     {
@@ -54,18 +42,30 @@ module.exports = [
         "clobbers": [
             "navigator.splashscreen"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "id": "phonegap-plugin-push.PushNotification",
+        "clobbers": [
+            "PushNotification"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.1",
-    "phonegap-plugin-push": "1.6.0",
-    "cordova-plugin-dialogs": "1.2.0",
     "cordova-plugin-device": "1.1.1",
+    "cordova-plugin-dialogs": "1.2.0",
     "cordova-plugin-network-information": "1.2.0",
     "cordova-plugin-splashscreen": "3.2.1",
-    "de.appplant.cordova.plugin.hidden-statusbar-overlay": "1.2.0"
+    "cordova-plugin-whitelist": "1.2.1",
+    "de.appplant.cordova.plugin.hidden-statusbar-overlay": "1.2.0",
+    "phonegap-plugin-push": "1.6.0"
 };
 // BOTTOM OF METADATA
 });
